fdcb2688dbbcc5bdbe4802efc0efdf926f9649c3
sadnalor <trip.rolandas@gmail.com>
2020-11-09 17:00:22 +0700

    passed data from clarizen to the app

--- scripts/saveManager.js
+++ scripts/saveManager.js
@@ -16,7 +16,7 @@ class SaveManager{
         this.groupedRawData = null;
         this.decryptedData = null;
         this.encryptionKey = "test";
-        console.log('sss');
+        console.log('ssss');
     }
 
     load = async () => {
